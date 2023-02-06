d28bbeb5f06ccf3b40cf47479012b88d1b81d6e7
Mariano Abel Puppo <MarianoPuppo@iMac-Mili-Sanchez.local>
2023-02-06 12:33:29 -0300

    CV-Terminado

---
+++ index.js
@@ -0,0 +1,114 @@
+function saludo() {
+    alert('Bienvenid@ a mi página  web -Curriculum Vitae-')
+}
+
+
+
+function agrandar() {
+    document.getElementById('linkedin').style.width = '40px';
+    document.getElementById('linkedin').style.height = '40px';
+}
+
+function achicar() {
+    document.getElementById('linkedin').style.width = '16px';
+    document.getElementById('linkedin').style.height = '16px';
+}
+
+
+function agrandar_i() {
+    document.getElementById('instagram').style.width = '40px';
+    document.getElementById('instagram').style.height = '40px';
+}
+function achicar_i() {
+    document.getElementById('instagram').style.width = '16px';
+    document.getElementById('instagram').style.height = '16px';
+}
+
+
+function agrandar_f() {
+    document.getElementById('facebook').style.width = '40px';
+    document.getElementById('facebook').style.height = '40px';
+}
+
+function achicar_f() {
+    document.getElementById('facebook').style.width = '16px';
+    document.getElementById('facebook').style.height = '16px';
+}
+
+
+
+function agrandar_l() {
+    document.getElementById('link').style.width = '40px';
+    document.getElementById('link').style.height = '40px';
+}
+
+function achicar_l() {
+    document.getElementById('link').style.width = '16px';
+    document.getElementById('link').style.height = '16px';
+}
+
+
+function agrandar_in() {
+    document.getElementById('ins').style.width = '40px';
+    document.getElementById('ins').style.height = '40px';
+}
+function achicar_in() {
+    document.getElementById('ins').style.width = '16px';
+    document.getElementById('ins').style.height = '16px';
+}
+
+
+function agrandar_fa() {
+    document.getElementById('face').style.width = '40px';
+    document.getElementById('face').style.height = '40px';
+}
+
+function achicar_fa() {
+    document.getElementById('face').style.width = '16px';
+    document.getElementById('face').style.height = '16px';
+}
+
+function cambiar() {
+    document.getElementById('foto_prim').src = "imagenes/imagen1.jpeg";
+}
+
+function volver() {
+    document.getElementById('foto_prim').src = "imagenes/imagen4.jpeg";
+}
+
+
+function remarcar() {
+    document.getElementById('for').style.textShadow = '1px 1px 10px';    
+}
+
+function volver_uno() {
+    document.getElementById('for').style.textShadow = '';
+}
+
+function remarcar_dos() {
+    document.getElementById('for2').style.textShadow = '1px 1px 10px';    
+}
+
+function volver_dos() {
+    document.getElementById('for2').style.textShadow = '';
+}
+
+function remarcar_tres() {
+    document.getElementById('for1').style.textShadow = '1px 1px 10px';    
+}
+
+function volver_tres() {
+    document.getElementById('for1').style.textShadow = '';
+}
+
+
+
+
+
+
+
+
+
+
+
+//    ' '
